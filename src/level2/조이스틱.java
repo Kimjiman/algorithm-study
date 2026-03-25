3da624c15c321f7ae230429f74302f09fbc8d27b
Kimjiman <kjmmaster@naver.com>
2026-03-25 17:58:09 +0900

    [Feature] 조이스틱 푸는중

--- src/level2/조이스틱.java
+++ src/level2/조이스틱.java
@@ -1,5 +1,7 @@
 package level2;
 
+import java.util.Arrays;
+
 /**
  * 문제: 조이스틱
  * 출처: https://school.programmers.co.kr/learn/courses/30/lessons/42860
@@ -16,12 +18,28 @@ public class 조이스틱 {
     public static void main(String[] args) {
         Solution sol = new Solution();
 
+        System.out.println(sol.solution("JAZ")); // 기댓값: 11
         System.out.println(sol.solution("JEROEN")); // 기댓값: 56
         System.out.println(sol.solution("JAN"));    // 기댓값: 23
     }
 
     static class Solution {
         public int solution(String name) {
+            // ABCDEFGHIJKLM
+            // NOPQRSTUVWXYZ
+            int[] nameCntArr = new int[name.length()];
+
+            for(int i = 0; i < nameCntArr.length; i++) {
+                nameCntArr[i] = ('M' - name.charAt(i) < 0) ? 'Z' - name.charAt(i) + 1 : name.charAt(i) - 'A';
+            }
+
+            System.out.println(Arrays.toString(nameCntArr));
+            while(true) {
+                
+
+                break;
+            }
+
             int answer = 0;
             return answer;
         }
